5972c53ece011dc9cc9f5bed858d92efd226353f
SagertStav <biryukovsrg@yandex.ru>
2023-07-09 10:05:16 +0300

    Исправил тесты (92%), gitignore (было *cashe*.* вместо *cache*.*), перевел функции из main в модуль func, poetry add pyunpack pytest pytest-cov

--- main.py
+++ main.py
@@ -4,79 +4,15 @@
 
 from pyunpack import Archive
 import pathlib
-from pathlib import Path
-import json
 
-transactions_file = Path(pathlib.Path.cwd(), '', '', 'src\operations.json')
+import func
 
-
-def read_actions(transactions_json_file):
-    ''' read_actions() считывает в память из json-файла транзакции клиента,
-     возвращая объект - загруженный список словарей с операциями по счетам/картам клиента '''
-    try:
-        with open(transactions_json_file, 'r', encoding='utf-8') as f:  # открыли файл с данными
-            # для run                               проходит низлежащий путь src\operations.json,
-            # а для тестов в терминале (poetry run pytest --cov) - только ..\src\operations.json  с двумя точками
-
-            return json.load(f)  # загнали все, что получилось в переменную
-    except FileNotFoundError:
-        with open(Path(pathlib.Path.cwd(), '', '', '..\src\operations.json'), 'r',
-                  encoding='utf-8') as f:  # открыли файл с данными
-            return json.load(f)  # загнали все, что получилось в переменную
-
-
-def last5_actions(client_transactions):
-    ''' Функция last5_actions() выбирает последние 5 УСПЕШНЫХ транзакций клиента,
-   возвращая отсортированный по убыванию дат операций список
-   :param client_transactions: - список словарей - всех транзакций клиента
-   :return: - выборка только 5-ти последних состоявшихся операций (список) '''
-
-    return sorted([x for x in client_transactions \
-                   if x.get('state') == "EXECUTED"], reverse=True, \
-                  key=lambda k: k['date'])[:5]  # топ 5-ти первых уже отсортированных по убыванию
-
-
-def my_dd_mm_yyyy_format(datetime_str):
-    ''' Функция, возвращающая из текстовой строку
-    (по формату внешнего источника-архива с операциями клиента)
-    с датой/временем операций - ДАТУ в формате ДД.ММ.ГГГГ '''
-
-    yyyy, mm, dd = datetime_str[:10].split('-')  # использую распаковку сразу в 3 переменные (год, месяц, день)
-    return f"{dd}.{mm}.{yyyy}"
-
-
-def masked_account(account_str):
-    ''' masked_account() маскирует полный вывод номера счета или карты
-   Для счета **XXXX - только последние 4 цифры.
-   Для карты XXXX XX** **** XXXX - видны первые 6 цифр и последние 4,
-   с пробелами между блоками по 4 цифры '''
-
-    return '' if account_str is None \
-        else ("счет **" + account_str[-4:]) if 'счет' == account_str[:4].lower() \
-        else account_str[:-14] + "** **** " + account_str[-4:]
-
-
-def chain_last_transactions(last_transactions, n=5):
-    ''' show_last_transactions() выводит на экран по умолчанию последние 5 состоявшихся транзакций:
-    дату, сумму, валюту операции, краткое описание, id операции, на какой счет и с какого (при наличии),
-    разделенные пустой строкой '''
-
-    print(last_transactions)
-    return "\n\n".join([f"{my_dd_mm_yyyy_format(last_transactions[i].get('date'))} сумма " +
-                        format(last_transactions[i].get('operationAmount')['amount'], " >12") + " " +
-                        format(last_transactions[i].get('operationAmount')['currency']['name'], " <4") +
-                        "- " + format(last_transactions[i].get('description'), " <25") +
-                        "№ оп." + format(last_transactions[i].get('id'), " >12") + ": " +
-                        f"{masked_account(last_transactions[i].get('to'))} " + \
-                        (("<= " + format(masked_account(last_transactions[i].get('from')), " >27")) \
-                             if 'from' in list(last_transactions[i].keys()) else '') for i in
-                        range(0, min(n, len(last_transactions)))])
 
 
 if __name__ == '__main__':
-    actions_source = Path(pathlib.Path.cwd(), '', '', 'src\operations.zip')
+    actions_source = pathlib.Path.cwd()/ 'src'/'operations.zip'
     # распакуем файл из архива (источник может и передаваться на вход в виде zip-файла)
     Archive(actions_source).extractall(pathlib.Path.cwd())
-    last_five_transactions = last5_actions(read_actions(transactions_file))
+    last_five_transactions = func.last5_actions(func.read_actions(func.transactions_file))
     # print(last_five_transactions)
-    print(chain_last_transactions(last_five_transactions))
+    print(func.chain_last_transactions(last_five_transactions))
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,13 +1,52 @@
 """ Тесты для виджета transactions """
-import pytest, main
+import pytest, main, pathlib
+import func
 
 
 def test_read_actions():
-    all_transactions = main.read_actions('src\operations.json')
+    #all_transactions = main.read_actions('src\operations.json')
+    all_transactions = func.read_actions(func.transactions_file)
     print(len(all_transactions))
     assert len(all_transactions) == 101  #по предложенному источнику для курсовой
 
+def test_my_dd_mm_yyyy_format():
+    assert func.my_dd_mm_yyyy_format("2019-08-26T10:50:58.294041") == "26.08.2019"
+
+def test_masked_account():
+    assert func.masked_account("Счет 64686473678894779589") == "счет **9589"
+
+def test_chain_last_transactions():
+ #   print(main.chain_last_transactions(main.last5_actions(main.read_actions('src\operations.json')),2))
+    s=func.read_actions(func.transactions_file)
+    s2=func.last5_actions(s)
+    print('s2:', s2)
+
+
+    print(func.chain_last_transactions(s2, 1))
+    assert func.chain_last_transactions(s2,1) == "08.12.2019 сумма     41096.24 USD - Открытие вклада          № оп.   863064926: счет **5907"
+
+def test_last5_actions():
+    s=func.read_actions(func.transactions_file)
+    s2 = func.last5_actions(s)
+    assert [float(x.get('operationAmount')['amount']) for x in s2] == [41096.24, 48150.39, 30153.72, 62814.53, 21344.35]
 
 
 
-test_read_actions
+"""
+"08.12.2019 сумма     41096.24 USD - Открытие вклада          № оп.   863064926: счет **5907
+
+07.12.2019 сумма     48150.39 USD - Перевод организации      № оп.   114832369: счет **3655 <= Visa Classic 28** **** 9012"
+"""
+
+print('Теытф: ',f"{(pathlib.Path.cwd().parent if pathlib.Path.cwd().name[-5:]=='tests' else pathlib.Path.cwd()) / 'src'/'operations.json'}")
+
+test_read_actions()
+test_my_dd_mm_yyyy_format()
+test_masked_account()
+
+
+
+test_chain_last_transactions()
+
+
+test_last5_actions()
